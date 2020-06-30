8cc8dba1a4ae7060db8fe51b920be2e3aaa4ad48
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-06-30 15:57:13 +0100

    make API call

---
+++ app.py
@@ -0,0 +1,13 @@
+import ast
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+r = requests.get("https://ci.marketplace.team/job/release-admin-frontend/api/python?tree=allBuilds["
+                 "number,id,timestamp,result,duration]",
+                 auth=(os.getenv('JENKINS_USERNAME'),
+                       os.getenv('JENKINS_API_TOKEN')))
+builds = ast.literal_eval(r.text)
+
+
