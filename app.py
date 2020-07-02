902f2acfe81cfd694090e0be5e336b4bf99bf9cd
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-07-02 12:52:40 +0100

    add all apps to csv

--- app.py
+++ app.py
@@ -6,25 +6,35 @@ import csv
 import datetime
 
 load_dotenv()
+pipelines = ['release-antivirus-api',
+             'release-admin-frontend',
+             'release-api',
+             'release-brief-responses-frontend',
+             'release-briefs-frontend',
+             'release-buyer-frontend',
+             'release-router',
+             'release-search-api',
+             'release-supplier-frontend',
+             'release-user-frontend',
+             ]
+
 
 def milliseconds_to_hours(milliseconds):
     td = datetime.timedelta(milliseconds=milliseconds)
-    return td.days, td.seconds//3600, (td.seconds//60) % 60
+    return td.days, td.seconds // 3600, (td.seconds // 60) % 60
 
-
-r = requests.get("https://ci.marketplace.team/job/release-admin-frontend/api/python?tree=allBuilds["
-                 "number,id,timestamp,result,duration]",
-                 auth=(os.getenv('JENKINS_USERNAME'),
-                       os.getenv('JENKINS_API_TOKEN')))
-builds = ast.literal_eval(r.text)
-successful_builds = list(filter(lambda b: b['result'] == 'SUCCESS', builds['allBuilds']))
-print(successful_builds)
 
 with open('output.csv', 'w') as csv_file:
     fieldnames = ['pipeline', 'run_id', 'duration']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
-    for sb in successful_builds:
-        writer.writerow({'pipeline': 'release-admin-frontend', 'run_id': sb['id'], 'duration': sb['duration']})
-
-
+    for pipeline in pipelines:
+        r = requests.get(f"https://ci.marketplace.team/job/{pipeline}/api/python?tree=allBuilds["
+                         "number,id,timestamp,result,duration]",
+                         auth=(os.getenv('JENKINS_USERNAME'),
+                               os.getenv('JENKINS_API_TOKEN')))
+        builds = ast.literal_eval(r.text)
+        successful_builds = list(filter(lambda b: b['result'] == 'SUCCESS', builds['allBuilds']))
+        for sb in successful_builds:
+            writer.writerow({'pipeline': pipeline, 'run_id': sb['id'], 'duration': sb['duration']})
+            print(successful_builds)
