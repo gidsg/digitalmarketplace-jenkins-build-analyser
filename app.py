25cf6f334eb53872283791d9858604418db4430b
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-07-02 12:27:25 +0100

    generate csv

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ import ast
 import requests
 from dotenv import load_dotenv
 import os
+import csv
+import datetime
+
 load_dotenv()
+
+def milliseconds_to_hours(milliseconds):
+    td = datetime.timedelta(milliseconds=milliseconds)
+    return td.days, td.seconds//3600, (td.seconds//60) % 60
+
 
 r = requests.get("https://ci.marketplace.team/job/release-admin-frontend/api/python?tree=allBuilds["
                  "number,id,timestamp,result,duration]",
@@ -11,5 +19,12 @@ r = requests.get("https://ci.marketplace.team/job/release-admin-frontend/api/pyt
 builds = ast.literal_eval(r.text)
 successful_builds = list(filter(lambda b: b['result'] == 'SUCCESS', builds['allBuilds']))
 print(successful_builds)
+
+with open('output.csv', 'w') as csv_file:
+    fieldnames = ['pipeline', 'run_id', 'duration']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    for sb in successful_builds:
+        writer.writerow({'pipeline': 'release-admin-frontend', 'run_id': sb['id'], 'duration': sb['duration']})
 
 
