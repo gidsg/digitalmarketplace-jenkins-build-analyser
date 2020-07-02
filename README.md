e55ebb82e0315aabb8c9f792ae0dcc4625157955
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-07-02 17:05:43 +0100

    print statistics and persist timestamp

---
+++ README.md
@@ -0,0 +1,12 @@
+# Jenkins build analyser
+
+## Setup
+
+`python -m venv ./venv`
+`source venv/bin/activate`
+`pip install -r requirements.txt`
+Populate `.env.example` and rename to `.env`
+
+## Running app
+
+
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import csv
 import datetime
+import statistics
 
 load_dotenv()
 pipelines = ['release-antivirus-api',
@@ -26,15 +27,17 @@ def time_formatter(milliseconds):
     return f"{hours} hours"
 
 
-with open('output.csv', 'w') as csv_file:
+filename = 'output.csv'
+
+with open(filename, 'w') as csv_file:
     all_successful_builds = list()
     row_count = 0
-    fieldnames = ['pipeline', 'run_id', 'duration']
+    fieldnames = ['pipeline', 'run_id', 'timestamp (UNIX)', 'duration (milliseconds)']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     for pipeline in pipelines:
         r = requests.get(f"https://ci.marketplace.team/job/{pipeline}/api/python?tree=allBuilds["
-                         "number,id,timestamp,result,duration]",
+                         "id,timestamp,result,duration]",
                          auth=(os.getenv('JENKINS_USERNAME'),
                                os.getenv('JENKINS_API_TOKEN')))
         builds = ast.literal_eval(r.text)
@@ -42,8 +45,13 @@ with open('output.csv', 'w') as csv_file:
         print(successful_builds)
         all_successful_builds.extend(successful_builds)
         for sb in successful_builds:
-            writer.writerow({'pipeline': pipeline, 'run_id': sb['id'], 'duration': sb['duration']})
-
+            writer.writerow({'pipeline': pipeline,
+                             'run_id': sb['id'],
+                             'timestamp (UNIX)': sb['timestamp'],
+                             'duration (milliseconds)': sb['duration']
+                             })
+    print(f"Results written to: {os.getcwd()}/{filename}")
     print("Total successful builds: " + str(len(all_successful_builds)))
-    average_duration = sum(list(map(lambda x: x['duration'], all_successful_builds))) / len(all_successful_builds)
-    print("Average total runtime: " + str(time_formatter(average_duration)))
+    duration_list = list(map(lambda x: x['duration'], all_successful_builds))
+    print("Average total runtime: " + time_formatter(statistics.mean(duration_list)))
+    print("Mean total runtime: " + time_formatter(statistics.median_low(duration_list)))
